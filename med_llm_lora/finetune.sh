e93e1aebb90b620635ad9c32bf853b4a54066482
Swastik N <swastiknath@icloud.com>
2024-04-27 22:33:09 +0530

    Added data  for Molecules

--- med_llm_lora/finetune.sh
+++ med_llm_lora/finetune.sh
@@ -1,6 +1,6 @@
 python finetune.py \
     --base_model 'decapoda-research/llama-7b-hf' \
-    --data_path '/path/to/med_alpaca_data_clean.json' \
+    --data_path './data/train.json' \
     --micro_batch_size 32 \
-    --output_dir './med-alpaca-lora' 
+    --output_dir './med_llm_lora' 
     
